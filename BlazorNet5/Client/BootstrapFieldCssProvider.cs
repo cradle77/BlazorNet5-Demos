2c2e705f20f34ecdb7b2f5c10955323de94bda71
cradle77 <marcodesanctis2@gmail.com>
2020-11-21 20:51:50 +0000

    custom css

---
+++ BlazorNet5/Client/BootstrapFieldCssProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Linq;
+
+namespace BlazorNet5.Client
+{
+    public class BootstrapFieldCssProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var isValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            return "form-control " + (isValid ? "is-valid" : "is-invalid");
+        }
+    }
+}
