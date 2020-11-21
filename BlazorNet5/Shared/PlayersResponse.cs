a2d557ee40409bb6435b0dbf722348093ba024da
cradle77 <marcodesanctis2@gmail.com>
2020-11-21 21:22:10 +0000

    virtualize

---
+++ BlazorNet5/Shared/PlayersResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorNet5.Shared
+{
+    public class PlayersResponse
+    {
+        public int TotalCount { get; set; }
+        public List<Player> Players { get; set; }
+    }
+}
